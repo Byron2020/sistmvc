354fa8b0180fbadb0df748b02165ecb3487130ac
Byron2020 <vini.byron@gmail.com>
2026-01-23 14:04:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mibrapp',
+  webDir: 'www'
+};
+
+export default config;
